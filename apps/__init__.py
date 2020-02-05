996cc9bae3fb4d375b32a981b3d9ac7500bfb817
sssunda <sundaeun93@gmail.com>
2020-02-06 00:09:04 +0900

    practice flask restful

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from models.database import init_db
 from account.views import account
+from account.views import api
+
 app = Flask(__name__)
 init_db()
-app.register_blueprint(account, url_prefix='/account/')
+
+api.init_app(account)
+app.register_blueprint(account, url_prefix='/account')
 
 @app.route("/")
 def hello():
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Flask, Blueprint
+from flask_restful import Resource, Api, reqparse
 
 account = Blueprint('views', __name__)
+api = Api()
 
-@account.route('/login')
-def login():
-    return "LOGIN!"
+
+class Login(Resource):
+    def get(self):
+        try:
+            return {'status': 'success'}
+        except Exception as e:
+            return {'error': str(e)}
+
+api.add_resource(Login,'/login')
