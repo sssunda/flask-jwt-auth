5bc0182cc55f3ae47392825e0c683d228a549cd9
sssunda <sundaeun93@gmail.com>
2020-02-06 23:59:23 +0900

    create login restapi

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask, session
 from models.database import init_db
-from account.views import account
-from account.views import api
+from account.views import account, api
 
 app = Flask(__name__)
 init_db()
 
 api.init_app(account)
 app.register_blueprint(account, url_prefix='/account')
+app.secret_key = 'super secret key'
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    try:
+        if session['logged_in']:
+            return "Success Login"
+    except:
+        return "Hello, World! Please Login!"
 
 if __name__ == '__main__':
   app.run()
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,5 +1,6 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, session, redirect, url_for
 from flask_restful import Resource, Api, reqparse
+from models.user import User
 
 account = Blueprint('views', __name__)
 api = Api()
@@ -11,5 +12,20 @@ class Login(Resource):
             return {'status': 'success'}
         except Exception as e:
             return {'error': str(e)}
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', required=True)
+        parser.add_argument('password', required=True)
+        args = parser.parse_args()
+
+        try:
+            data = User.query.filter_by(username=args['username'], password=args['password']).first()
+            if data is not None:
+                session['logged_in'] = True
+
+            return redirect(url_for('hello'))
+        except Exception as e:
+            return {'error': str(e)}
+
 
 api.add_resource(Login,'/login')
